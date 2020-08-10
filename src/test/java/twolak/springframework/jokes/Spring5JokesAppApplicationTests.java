dece43c58b4fcbdde2b4477567696d971e74dd86
twolak88 <twolak88@gmail.com>
2020-08-10 23:59:28 +0200

    initial project

---
+++ src/test/java/twolak/springframework/jokes/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package twolak.springframework.jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
